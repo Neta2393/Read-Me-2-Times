69a6a30f4d3055471d14643be5ec6c655fd94837
Neta2393 <benetheahardin@yahoo.com>
2023-06-04 11:42:45 -0700

    Added functions on lines 1-5 of index.js file

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const utils = require('utils');
+
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
