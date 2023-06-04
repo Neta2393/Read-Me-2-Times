1ab1dd8557f701b0f0ff41829e05d2b1c39b87cd
Neta2393 <benetheahardin@yahoo.com>
2023-06-04 14:13:49 -0700

    Added questions array and changed license badge and added license badges

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
-const generateMarkdown = require("./util/generateMarkdown");
-const questions = require("./util/questions").questions;
+const generateMarkdown = require('./util/generateMarkdown');
+const licenseBadge = require('./util/licensebadge');
+const questions = require('./util/questions').questions;
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
